2295068650ffbd28b60e604daa280add147292d0
jonarro0 <jrodriguez@cloudgenia.com>
2024-10-14 11:32:56 -0600

    feat: add layout crm

--- client/src/components/templates/header.tsx
+++ client/src/components/templates/header.tsx
@@ -6,8 +6,8 @@ import { UserNav } from './user-nav';
 export default function Header() {
   return (
     <header className="sticky inset-x-0 top-0 w-full">
-      <nav className="flex items-center justify-between px-4 py-2 md:justify-end">
-        <div className={cn('block lg:!hidden')}>
+      <nav className="flex items-center justify-between px-4 py-2 md:justify-end flex-row">
+        <div className={cn('block md:!hidden')}>
           <MobileSidebar
             navItems={[
               {
--- client/src/components/templates/user-nav.tsx
+++ client/src/components/templates/user-nav.tsx
@@ -14,54 +14,60 @@ import {
 import { signOut, useSession } from '@/hooks/useAth';
 export function UserNav() {
   const { user } = useSession();
-  if (user) {
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-            <Avatar className="h-8 w-8">
-              <AvatarImage
-                src={`http://localhost:5000/users/${user.id}.webp`}
-                alt={user.name}
-              />
-              <AvatarFallback>{user.name[0]}</AvatarFallback>
-            </Avatar>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">
-                {user.name}
-              </p>
-              <p className="text-xs leading-none text-muted-foreground">
-                {user.email}
-              </p>
-            </div>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              Profile
-              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Billing
-              <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Settings
-              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>New Team</DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => signOut()}>
-            Log out
-            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-8 w-8">
+            {user ? (
+              <>
+                <AvatarImage
+                  src={`http://localhost:5000/users/${user.id}.webp`}
+                  alt={user.name}
+                />
+                <AvatarFallback>{user.name[0]}</AvatarFallback>
+              </>
+            ) : (
+              <AvatarFallback>U</AvatarFallback>
+            )}
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">
+              {user ? user.name : 'User'}
+            </p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {user ? user.email : ' '}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
           </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    );
-  }
+          <DropdownMenuItem>
+            Billing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>New Team</DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>
+          Log out
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+
 }
